51f7bdeac3b2aea98b736f60aed4eda97485240b
SHS <SHS@shs.com>
2019-11-24 01:25:30 +0500

    Test

--- DoxcFilling/Main.py
+++ DoxcFilling/Main.py
@@ -1,1 +1,15 @@
-import
+from docxtpl import DocxTemplate
+
+doc_name = 'DogovorGPKh'
+file_path = "/home/god/Hantaton/hantaton-team/DoxcFilling/"
+
+
+doc = DocxTemplate(f"{file_path}{doc_name}.docx")
+context = { 'CompanyCity' : "Ханты"}
+doc.render(context)
+
+doc.save(f"{file_path}шаблон-final.pdf")
+
+# context = { 'director' : "И.И.Иванов"}
+# doc.render(context)
+# doc.save("шаблон-final.docx")
