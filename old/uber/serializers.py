0b1e3a1819c52ca9a421dc9e281321da3f1056e2
evgen <user062199@gmail.com>
2019-11-24 08:07:19 +0500

    yolo bicha

---
+++ old/uber/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import *
+
+# class BonusTransactionSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = BonusTransaction
+#         fields = ['id', 'user', 'account_payment', 'value', 'note'] #'date_created', 'date_updated']
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['phone']# 'date_created', 'date_updated']
+
+# class BonusTransactionSerializer(serializers.ModelSerializer):
+#     supplier_service = serializers.SerializerMethodField()
+#     date_created = serializers.DateTimeField()
+
+#     class Meta:
+#         model = BonusTransaction
+#         fields = ['id', 'user', 'value', 'note', 'date_created', 'supplier_service']
+
+#     def get_supplier_service(self, obj):
+#         result_dict = {}
+#         try:
+#             for item in AccountPaymentService.objects.filter(account_payment=obj.account_payment.id):
+#                 result_dict[item.supplier_name] = item.service_name
+#         except Exception: pass
+#         if result_dict: return result_dict
+#         else: return None
+            
